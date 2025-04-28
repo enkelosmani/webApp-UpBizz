45c92945b3a10c7bfad2eeed99d0ccc32ca4f224
Enkel <enkel.osmani1@gmail.com>
2025-04-29 01:54:17 +0200

    'webApplication'

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,6 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
-            boxShadow: {
-                '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
-            },
         },
     },
 
